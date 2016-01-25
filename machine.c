c4168bdf68528d8ae2e84732c6208bb48ab0fdac
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-01-24 21:35:48 -0600

    Start implementing interpreter & corresponding functions

---
+++ machine.c
@@ -0,0 +1,113 @@
+#include "machine.h"
+
+
+
+/* Uses value in IR to determine course of action */
+/* Returns false if errors */
+bool interpreter() 
+{
+	machine.PC++; /* Increment Program Counter */
+	short int op = getOpcode(machine.IR);
+	switch (op) {
+		case 0:  return LOD(); break;
+		case 1:  return STO(); break;
+		case 2:  return ADD(); break;
+		case 3:  return SUB(); break;
+		case 4:  return ADR(); break;
+		case 5:  return SUR(); break;
+		case 6:  return AND(); break;
+		case 7:  return IOR(); break;
+		case 8:  return NOT(); break;
+		case 9:  return JMP(); break;
+		case 10: return JEQ(); break;
+		case 11: return JGT(); break;
+		case 12: return JLT(); break;
+		case 13: return CMP(); break;
+		case 14: return CLR(); break;
+		case 15: return HLT(); break;
+		default: return false; break;
+	}
+}
+
+/* Retrieves the opcode */
+/* Returns an int 0-15 */
+short int getOpcode(short int num)
+{
+	return (num & 61440)>>12;
+}
+
+/* Retrieves the address mode */
+/* Returns 0 or 1 */
+short int getAddrMode(short int num)
+{
+	return (num & 2048)>>11;
+}
+
+/* Retrieves the register code */
+/* Returns an int 0-3 */
+short int getRegCode(short int num)
+{
+	return (num & 1792)>>8;
+}
+
+/* Retrieves the operand */
+/* Returns an int 0-255 */
+short int getOperand(short int num)
+{
+	return (num & 255);
+}
+
+/* Loads a value into a register */
+bool LOD() 
+{
+	short int addr = getAddrMode(machine.IR);
+	short int operand = getOperand(machine.IR);
+	short int reg = getRegCode(machine.IR);
+
+	if (addr == 0) {
+		switch (reg) {
+			case 0: machine.rA = getOperand(machine.IR); break;
+			case 1: machine.r1 = getOperand(machine.IR); break;
+			case 2: machine.r2 = getOperand(machine.IR); break;
+			case 3: machine.r3 = getOperand(machine.IR); break;
+			default: return false; break;
+		};
+	} else if (addr == 1) {
+		switch (reg) {
+			case 0: machine.rA = main_memory[getOperand(machine.IR)]; break;
+			case 1: machine.r1 = main_memory[getOperand(machine.IR)]; break;
+			case 2: machine.r2 = main_memory[getOperand(machine.IR)]; break;
+			case 3: machine.r3 = main_memory[getOperand(machine.IR)]; break;
+			default: return false; break;
+		};
+	} else {
+		return false;
+	}
+	return true;
+}
+bool STO() 
+{
+	short int reg = getRegCode(machine.IR);
+	switch (reg) {
+		case 0: main_memory[getOperand(machine.IR)] = machine.rA; break;
+		case 1: main_memory[getOperand(machine.IR)] = machine.r1; break;
+		case 2: main_memory[getOperand(machine.IR)] = machine.r2; break;
+		case 3: main_memory[getOperand(machine.IR)] = machine.r3; break;
+		default: return false; break;
+	};
+	return true;
+}
+bool ADD() {return false;}
+bool SUB() {return false;}
+bool ADR() {return false;}
+bool SUR() {return false;}
+bool AND() {return false;}
+bool IOR() {return false;}
+bool NOT() {return false;}
+bool JMP() {return false;}
+bool JEQ() {return false;}
+bool JGT() {return false;}
+bool JLT() {return false;}
+bool CMP() {return false;}
+bool CLR() {return false;}
+bool HLT() {return false;}
---
+++ machine.h
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include "binops.h"
+
+#ifndef MACHINE
+#define MACHINE
+
+/* Define boolean type */
+typedef enum { false, true } bool;
+
+/* Define registers type */
+typedef struct registers {
+	short int r1, r2, r3, rA, IR, PC:8;
+} registers;
+
+/* Declare and initialize machine registers */
+registers machine = {
+	.r1 = 0,
+	.r2 = 0,
+	.r3 = 0,
+	.rA = 0,
+	.IR = 0,
+	.PC = 0
+};
+
+/* Declare main memory */
+short int main_memory[256];
+
+/* Uses value in IR to determine course of action */
+/* Returns false if errors */
+bool interpreter();
+
+/* Retrieves the opcode */
+/* Returns an int 0-15 */
+short int getOpcode(short int num);
+
+/* Retrieves the address mode */
+/* Returns 0 or 1 */
+short int getAddrMode(short int num);
+
+/* Retrieves the register code */
+/* Returns an int 0-3 */
+short int getRegCode(short int num);
+
+/* Retrieves the operand */
+/* Returns an int 0-255 */
+short int getOperand(short int num);
+
+bool LOD();
+bool STO();
+bool ADD();
+bool SUB();
+bool ADR();
+bool SUR();
+bool AND();
+bool IOR();
+bool NOT();
+bool JMP();
+bool JEQ();
+bool JGT();
+bool JLT();
+bool CMP();
+bool CLR();
+bool HLT();
+
+#endif
