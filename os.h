44da1ae4a27774605d87e3509d2b34ce66939bbe
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-01-26 16:24:47 -0600

    Initialize user instruction registers

--- os.h
+++ os.h
@@ -6,6 +6,17 @@
 #define OS
 
 registers U1, U2, SYS;
+U1.IR = 61440;
+U2.IR = 61440;
+/* 
+Note: U1 & U2 instruction registers contain 61440 (HLT) 
+while user is not running any program. The run function
+will later assign the IR to the value of the starting
+memory address of the program.
+   
+The scheduler can quickly determine if a user is running
+something by testing the opcode of the IR for HLT (1111).
+*/
 
 /* Dump contents of main memory */
 void dump(bool dumpRegs);
