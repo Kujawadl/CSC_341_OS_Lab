564ce5749dccbfb3ef16d303248ecad5dceb6cf9
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-02-09 21:05:52 -0600

    Change comment style

--- os.c
+++ os.c
@@ -26,18 +26,17 @@ int main(int argc, char** argv)
 void readFile(){
     /* read input file into int main_memory[] */
     FILE *ifp;
-    ifp = fopen("part1.dat", "r"); // input file
+    ifp = fopen("part1.dat", "r"); /* input file */
 
     /* validates file exists */
     if (ifp == NULL){perror("proj1a.dat");exit(0);}
 
-    char str[32];//current line from file, had to increase it size...
-    int i = 0; //index of main_memory
-    while(fgets(str, 18, ifp)){ // plus 2 for new lines.
-        // printf("%s\t", str);//DEBUG
+    char str[32]; /* current line from file, had to increase it size... */
+    int i = 0; /* index of main_memory */
+    while(fgets(str, 18, ifp)){ /* plus 2 for new lines. */
+        /* printf("%s\t", str); */ /* DEBUG */
         main_memory[i] = (short)(strtoul(str, NULL, 2));
-        // printf("%d\n", main_memory[i]);//DEBUG
-        printbits(main_memory[i]);
+        /* printf("%d\n", main_memory[i]); */ /* DEBUG */
         i++;
     }
     fclose(ifp);
