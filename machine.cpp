30fd885e24b6412e1caf401c07523f7f5a196ec2
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-04-12 09:00:30 -0500

    Add usleep()

--- machine.cpp
+++ machine.cpp
@@ -55,6 +55,7 @@ bool interpreter() {
 	bool success = true;
 	//While no error flag and no timer interrupt
 	while (success && timer_interrupt < QUANTUM) {
+		usleep(1500000); // Sleep 1.5 seconds to clearly demonstrate quantum
 		machine.IR = main_memory[MMU(machine.PC)];
 		machine.PC++; // Increment Program Counter
 		unsigned short int op = getOpcode(machine.IR);
