a0f20115a352d4e7146f50ae0705143ee87eb5f1
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2016-04-27 23:36:56 -0500

    Add missing semicolons; fix typedef

--- fsys.hpp
+++ fsys.hpp
@@ -6,33 +6,31 @@
  * Spring 2016
  */
 
- #include <iostream> // cout, cerr
- #include <iomanip>  // setw, setfill, left, right, dec, endl
- #include <sstream>  // stringstream
- #include <vector>   // we need vectors
-  using namespace std;
+#include <iostream> // cout, cerr
+#include <iomanip>  // setw, setfill, left, right, dec, endl
+#include <sstream>  // stringstream
+#include <vector>   // we need vectors
+using namespace std;
 
- // Lets go ahead and get that disk declared
+// Lets go ahead and get that disk declared
 
 
- class FSYS {
-  public:
-    FSYS();
-    void print();
-    string toString();
-    bool save();
-    bool load();
-    //Look up Fucking String array overloarding and DO IT
-  private:
-    unsigned short int disk[512];
-    // Records will contain all pertinent information to find files
-    //  in disk and will be dynamically added to the FAT.
-    struct FAT_FUCKING_Record {
-      string fileName;
-      short int location, size;
-    }
-    typedef FAT {
-      vector<FAT_FUCKING_Record> arrList;
-    }
+class FSYS {
+public:
+  FSYS();
+  void print();
+  string toString();
+  bool save();
+  bool load();
+  //Look up Fucking String array overloarding and DO IT
+private:
+  unsigned short int disk[512];
+  // Records will contain all pertinent information to find files
+  //  in disk and will be dynamically added to the FAT.
+  struct FAT_FUCKING_Record {
+    string fileName;
+    short int location, size;
+  };
+  typedef vector<FAT_FUCKING_Record> FAT ;
 
- }
+};
